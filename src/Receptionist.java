1e557bb316a0552b047a0e7f37466a7667fe220a
mirellesine <mirellesine@gmail.com>
2024-05-04 19:41:56 -0300

    Mover responsabilidades de check-in/check-out para Receptionist

--- src/Receptionist.java
+++ src/Receptionist.java
@@ -1,1 +1,15 @@
-public class Receptionist extends Thread { }
+public class Receptionist extends Thread {
+    public Receptionist() {}
+
+    public void checkGuestIn(Guest guest) {
+        // Atualizar isso para pegar um quarto vago e atualizar seu status para IN_USE
+        Room room = new Room();
+        guest.setRoom(room);
+        guest.start();
+    }
+
+    public void checkGuestOut(Guest guest) {
+        guest.interrupt();
+        guest.setRoom(null);
+    }
+}
